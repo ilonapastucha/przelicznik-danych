46a4bdd8fffda6d0585fddd8bc09282807fc8ee8
Ilona Pastucha <ilonka.p4@gmail.com>
2018-08-07 13:27:45 +0200

    fixed some file

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ var btn = document.getElementById('green-button');
 var convert; 
 
 var write = document.getElementById('green-output');
+writer.innerHTML = 'Click the button to convert!' + '<br><br>' + write.innerHTML;
 
 button.addEventListener('click', function(){
 	temperature = window.prompt('What is the temperature in Celsius?');
@@ -32,7 +33,7 @@ button.addEventListener('click', function(){
 		output.innerHTML = 'At this temperature we have summer' + '<br><br>' + output.innerHTML;
 	}
 	
-	if ( temperature === '' || temperature == null || temperature === NaN ){
+	if (temperature === '' || temperature == null || temperature === NaN) {
 	output.innerHTML = 'No value entered!' + '<br><br>' + output.innerHTML;
 	}
 	else {
@@ -43,26 +44,26 @@ button.addEventListener('click', function(){
 btn.addEventListener('click', function(){
 	convert = window.prompt('What is the temperature in Fahrenheit?');
 	
-	/* if (temperature <= 0) {
-		output.innerHTML = 'State of water concentration: ice.' + '<br><br>' + output.innerHTML;
-	} else if (temperature <100) {
-		output.innerHTML = 'State of water concentration: liquid.' + '<br><br>' + output.innerHTML;
-	} else if (temperature >= 100) {
-		output.innerHTML = 'State of water concentration: gas.' + '<br><br>' + output.innerHTML;
+	if (convert <= 32) {
+		write.innerHTML = 'State of water concentration: ice.' + '<br><br>' + write.innerHTML;
+	} else if (convert < 212) {
+		write.innerHTML = 'State of water concentration: liquid.' + '<br><br>' + write.innerHTML;
+	} else if (convert >= 212) {
+		write.innerHTML = 'State of water concentration: gas.' + '<br><br>' + write.innerHTML;
 	}
 	if (temperature  <=5) {
-		output.innerHTML = 'At this temperature we have winter.' + '<br><br>' + output.innerHTML;
-	} else if (temperature >5 && temperature <=15) {
-		output.innerHTML = 'At this temperature we have autumn.' + '<br><br>' + output.innerHTML;
+		write.innerHTML = 'At this temperature we have winter.' + '<br><br>' + write.innerHTML;
+	} else if (convert >5 && convert <=15) {
+		write.innerHTML = 'At this temperature we have autumn.' + '<br><br>' + writet.innerHTML;
 	} else if (temperature > 15) {
-		output.innerHTML = 'At this temperature we have summer' + '<br><br>' + output.innerHTML;
-	} */
+		write.innerHTML = 'At this temperature we have summer' + '<br><br>' + write.innerHTML;
+	}
 	
-	if ( convert === '' || convert == null || convert === NaN ){
-	write.innerHTML = 'No value entered!' + '<br><br>' + output.innerHTML;
+	if ( convert === '' || convert == null || convert === NaN){
+	write.innerHTML = 'No value entered!' + '<br><br>' + write.innerHTML;
 	}
 	else {
-	write.innerHTML = 'Your temperature in Fahrenheit is '+convert+' in Celsius is '+((convert - 32)/ 1.8)+ '!' + '<br><br>' + output.innerHTML; 
+	write.innerHTML = 'Your temperature in Fahrenheit is '+convert+' in Celsius is '+((convert - 32)/ 1.8)+ '!' + '<br><br>' + write.innerHTML; 
 	}
 }); 
 
